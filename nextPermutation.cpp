b12a2b7d0b0ff2d770476e93291f5c691a5d7673
Jinia Bhaumik <bhaumikjinia@gmail.com>
2025-12-06 23:49:08 +0530

    Next permutation

---
+++ nextPermutation.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Find the next permutation of the given array
+// Optimal
+
+vector<int> rearrangeArray(vector<int>&arr){
+    int n=arr.size();
+    int index=-1;
+    for(int i=n-2;i>=0;i--){
+        if(arr[i]<arr[i+1]){
+             index=i;
+            break;
+        }
+    }
+
+    if(index==-1){
+        reverse(arr.begin(),arr.end());
+        return arr;
+    }
+
+    for(int i=n-1;i>=index+1;i--){
+        if(arr[i]>arr[index]){
+            swap(arr[index],arr[i]);
+            break;
+        }
+        
+    }
+   
+    reverse(arr.begin()+index+1,arr.end());
+
+    return arr;
+}
+
+int main(){
+    vector<int>arr={2,1,5,4,3,0,0};
+    vector<int>a=rearrangeArray(arr);
+    for(auto it: a){
+        cout<< it<<" ";
+    }
+    return 0;
+}
