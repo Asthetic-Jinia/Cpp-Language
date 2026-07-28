9c2e2b66fef95e54e48f24a1c4d9355df3dcaa54
Jinia Bhaumik <bhaumikjinia@gmail.com>
2026-07-28 09:11:22 +0530

    Time: 0 ms (100%), Space: 12 MB (25.97%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         int s=strs.size();
+        /*
         string prefix=strs[0];
         for(int x=1; x<s; x++){
             while(strs[x].find(prefix) != 0){ // means substring is not found at 0th index
@@ -9,6 +10,19 @@ public:
                 if(prefix.empty()){
                     return "";
                 }
+            }
+        } */
+
+        string prefix="";
+        sort(strs.begin(),strs.end());
+        int i=0;
+        while(i<strs[0].size() && i<strs.back().size()){ // compare the first and last string
+            if(strs[0][i]==strs.back()[i]){
+                prefix.push_back(strs[0][i]);
+                i++;
+                
+            }else{
+                break;
             }
         }
         
